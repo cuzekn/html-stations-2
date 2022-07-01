0423fd619af9698452aedf7066da6c57343f61d8
cuze <cuzekn@gmail.com>
2022-07-01 22:36:28 +0900

    style: check -> textに変更

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,6 +1,6 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-  if (check.checked == false) {
+  if (text.checked == false) {
     document.body.style.backgroundColor = "#ffffff";
   } else {
     document.body.style.backgroundColor = "#ff0000";
