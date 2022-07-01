0370d0cea115a512f5a5f769033fd7a8f6a47b38
cuze <cuzekn@gmail.com>
2022-07-01 22:27:15 +0900

    style: if文にtrueを追加

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,6 +1,6 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-  if (check.checked) {
+  if (check.checked == true) {
     document.body.style.backgroundColor = "#ff0000";
   } else {
     document.body.style.backgroundColor = "#ffffff";
