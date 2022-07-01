2badbbe46ceab7db713207ce9fad5143d16f093c
cuze <cuzekn@gmail.com>
2022-07-01 22:18:55 +0900

    refactor: e.target -> checkに修正

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,6 +1,6 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-  if (e.target.checked) {
+  if (check.checked) {
     document.body.style.backgroundColor = "red";
   } else {
     document.body.style.backgroundColor = "white";
