ef3d00ef711a93a975aa7df1442545c3730fa421
cuze <cuzekn@gmail.com>
2022-07-01 22:31:13 +0900

    style: if文の構文を入れ換え

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,8 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-  if (check.checked == true) {
-    document.body.style.backgroundColor = "#ff0000";
-  } else {
+  if (check.checked == false) {
     document.body.style.backgroundColor = "#ffffff";
+  } else {
+    document.body.style.backgroundColor = "#ff0000";
   }
 }
