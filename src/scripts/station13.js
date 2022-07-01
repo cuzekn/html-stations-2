ff9dc1f50816a8402da6163c6d98fd8eda7925d0
cuze <cuzekn@gmail.com>
2022-07-01 21:24:27 +0900

    style: ボタンで背景色の変更

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if (e.target.checked) {
+    document.body.style.backgroundColor = "red";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
 }
