e28438adcf2e43057dce3e5a9118326341618348
cuze <cuzekn@gmail.com>
2022-07-01 22:24:03 +0900

    style: 色指定をカラーコードに修正

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,8 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
   if (check.checked) {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "#ff0000";
   } else {
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "";
   }
 }
